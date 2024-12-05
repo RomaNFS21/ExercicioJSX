042fcb49455db8d6a7601b7487c7002c2fd9331d
Victor Roma <66295991+RomaNFS21@users.noreply.github.com>
2024-12-04 23:42:19 -0300

    Primeira configuração do Header e TaskContainer

---
+++ task-manager/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header () {
+    return (
+        <React.Fragment>
+            <div>
+                <h1>
+                    Atividades do dia !
+                </h1>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Header
